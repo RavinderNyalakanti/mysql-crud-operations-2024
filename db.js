8a96068014520ed56c570683f52ee93f44c2813d
ravinder nyalakanti <127651669+RavinderNyalakanti@users.noreply.github.com>
2024-05-19 07:59:32 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mysql = require('mysql2')
 require('dotenv').config(); 
 
 const connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST, 
+        host: process.env.MYSQL_HOST,  
+        port:process.env.PORT,
         user: process.env.MYSQL_USER, 
         password: process.env.MYSQL_PASSWORD, 
         database: process.env.MYSQL_DATABASE
